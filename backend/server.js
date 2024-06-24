21ffcedeab3a8c4c4b1fcd2eaeb786d41a2dd3f9
Ritik Rajput <ritiksinghrajput1101@gmail.com>
2024-06-24 13:25:14 +0530

    added code

--- backend/server.js
+++ backend/server.js
@@ -19,14 +19,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
-app.use("/", (req,res)=>{
-	res.send("Hello There")
-});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 
 
